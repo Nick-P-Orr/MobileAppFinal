1b34b503c2e16897bb0f060d77a0f6b1b824f8b7
Nick <nick.p.orr@gmail.com>
2019-07-09 09:05:35 -0400

    Fixed issue in manifest with naming

--- app/src/main/java/com/example/notely/AllNotes.java
+++ app/src/main/java/com/example/notely/AllNotes.java
@@ -1,4 +1,14 @@
 package com.example.notely;
 
-public class AllNotes {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject);
+    }
+
 }
