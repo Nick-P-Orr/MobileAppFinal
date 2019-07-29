a78d90db2bc4b3d6186666b2aeeded8bcd55e57b
Nick <nick.p.orr@gmail.com>
2019-07-29 12:37:42 -0400

    Commiting unfinished code that should help with font changes

--- app/src/main/java/com/example/notely/ChangeFont.java
+++ app/src/main/java/com/example/notely/ChangeFont.java
@@ -1,6 +1,7 @@
 package com.example.notely;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -8,9 +9,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 
-public class ChangeFont extends AppCompatActivity {
+public class ChangeFont extends AppCompatActivity implements OnClickListener{
 
     private TextView mTextMessage;
     private static final String TAG = "MyActivity";
@@ -18,6 +21,8 @@ public class ChangeFont extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.font_change);
         Log.v("myApp", "In font change");
 
@@ -34,17 +39,14 @@ public class ChangeFont extends AppCompatActivity {
             public void onClick(View v) {
                 Log.v("myApp", "Activity2 button is clicked");
                 Intent intent = new Intent();
+                intent.setClass(ChangeFont.this, ChangeFont.class);
+                setTheme(R.style.SmallRoboto);
 
-
-
+                startActivity(intent);
 
 
             }
         });
-
-
-
-
 
 
         goback.setOnClickListener(new View.OnClickListener() {
@@ -67,4 +69,22 @@ public class ChangeFont extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.roboto_small:
+                Utils.changeToTheme(this, Utils.SmallRoboto);
+                break;
+            case R.id.button2:
+                Utils.changeToTheme(this, Utils.THEME_WHITE);
+                break;
+            case R.id.button3:
+                Utils.changeToTheme(this, Utils.THEME_BLUE);
+                break;
+        }
+    }
+
 }
